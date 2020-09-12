e874c07c7d245122cdc78c472b255e12407157a2
dani2110 <dpiedrahita259@misena.edu.co>
2020-09-12 10:24:24 -0500

    'Factory'

--- larapp/database/factories/UserFactory.php
+++ larapp/database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'fullname'          => $faker->name($gender),
         'email'             => $faker->unique()->safeEmail,
         'phone'             => $faker->numberBetween($min = 3101000000, $max = 3202000000),
-        'birthdate'         => $faker->dateTimeBetween($startDate = '-60 years', $endDate ='21 years', $timezone =null),
+        'birthdate'         => $faker->dateTimeBetween($startDate = '-60 years', $endDate ='-21 years', $timezone =null),
         'gender'            => $gender,
         'photo'             => '/storage/images/'.$faker->image('public/storage/images', 640, 480, null, false),
         'address'           => $faker->address,
